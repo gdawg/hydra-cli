d6edc8c6b98e5ca96495929aa7ed8a75413cfc62
Steven Degutis <steven@cleancoders.com>
2014-07-21 16:03:33 -0500

    Updated SHA1.

--- hydra-cli.rb
+++ hydra-cli.rb
@@ -3,8 +3,7 @@ require "formula"
 class HydraCli < Formula
   homepage 'https://github.com/sdegutis/hydra-cli'
   url 'https://github.com/sdegutis/hydra-cli/archive/1.0.tar.gz'
-  sha1 '4df0648db97c426ecb0965ffeefefaea40bae7ef'
-
+  sha1 'e514b3ef7e2207899e5a3cbf0a98b58c93ac00da'
   head 'https://github.com/sdegutis/hydra-cli.git'
 
   def install
